d5be426277b1aba0e32462cfb9c86e0bb897eb50
Ehsan Moradi <ehsan.hightech@gmail.com>
2024-10-23 13:23:33 +0330

    2 side wave , pixel and vertex shader

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Task2 from "./task2";
+
+function App() {
+  return (
+    <div className="App">
+      <Task2/>
+    </div>
+  );
+}
+
+export default App;
